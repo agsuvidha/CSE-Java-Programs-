78ee60c533333825daf96916c7b1a7b69975c7da
Suvidha Agarwal <67759335+agsuvidha@users.noreply.github.com>
2025-04-16 10:59:27 +0530

    Update app2.java

--- app2.java
+++ app2.java
@@ -8,11 +8,8 @@ class app2
             Class.forName("com.mysql.cj.jdbc.Driver");  
             Connection con=DriverManager.getConnection(  
             "jdbc:mysql://localhost:3306/","root","root1234");  
-            //here sonoo is database name, root is username and password  
             Statement stmt=con.createStatement();  
             stmt.execute("insert into mydemo.user values(2,'mno',18,'tester')");  
-            // while(rs.next())  
-            // System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
             System.out.println("Success");
             con.close();  
         }
