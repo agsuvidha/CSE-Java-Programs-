12e6254d7798fe58705c0a195da79c5f96d3fb69
Suvidha Agarwal <67759335+agsuvidha@users.noreply.github.com>
2025-04-16 10:59:11 +0530

    Update App.java

--- App.java
+++ App.java
@@ -5,11 +5,8 @@ try{
     Class.forName("com.mysql.cj.jdbc.Driver");  
     Connection con=DriverManager.getConnection(  
     "jdbc:mysql://localhost:3306/","root","root1234");  
-    //here sonoo is database name, root is username and password  
     Statement stmt=con.createStatement();  
     stmt.executeUpdate("create database mydemo");  
-    // while(rs.next())  
-    // System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
     System.out.println("Success");
     con.close();  
 }
